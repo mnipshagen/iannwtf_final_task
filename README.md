11416948db5ca9b610fb944114829ccbb199037d
Moritz Nipshagen <moritz.nipshagen@protonmail.com>
2018-02-24 20:59:35 +0100

    ing2id is here to stay

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This project was developed in the context of the course **Implementing Artificia
 
 The code was developed and tested with
 * python 3.6
-* tensorflow 1.5
+* tensorflow 1.5 & 1.3
 * MongoDB 3.6
 
 ## Install & Use
@@ -15,6 +15,7 @@ The code was developed and tested with
     * The application expects a local server running at `mongodb://localhost:27017`
     * The server either needs to host a database named `iannwtf` and a collection `recipes` or the `build_db.py` (see below) script can crawl and build the database from scratch. This takes a while.
     * A backup of the database can be downloaded from [Mo's Onedrive](https://1drv.ms/f/s!Am3LtCW8Ozvuh7J19osxy3B-qIucfQ) and restored with the `mongorestore` application bundled with the MongoDB server installation. The syntax is `mongorestore -d iannwtf <directory_backup>`. Also refer to [the documentation](https://docs.mongodb.com/manual/reference/program/mongorestore/).
+* The Jupyter-Notebook file holds the actual network code.
 
 ## What this is
 
--- src/recipes.py
+++ src/recipes.py
@@ -22,30 +22,52 @@ class Recipes:
     self.coll = self.db[RECIPE_COLLECTION]
 
   def create_dictionaries(self):
-    pass
+    agg = self.coll.aggregate([
+        {"$project": {"_id": 0, "ingredients.foodId": 1, "ingredients.name": 1}},
+        {"$unwind": "$ingredients"},
+        {
+            "$group": {
+                "_id": "$ingredients.foodId",
+                "name": {"$first": "$ingredients.name"}
+            }
+        }
+      ], allowDiskUse=True)
 
-  def ing2ids(self):
-    pass
+    self._ing2id = {item['name']: item['_id'] for item in agg}
+    self._id2ing = dict(zip(self._ing2id.values(), self._ing2id.keys()))
+    self._word2id = {}
+    self._id2word = {} 
+    self._unit2id = {}
+    self._id2unit = {}
+
+  def ings2ids(self, ings):
+    return self._2(ings, self._ing2id)
   
-  def unit2ids(self):
-    pass
+  def units2ids(self, units):
+    return self._2(units, self._unit2id)
 
-  def words2ids(self):
-    pass
+  def words2ids(self, words):
+    return self._2(words, self._word2id)
 
-  def ids2ing(self):
-    pass
+  def ids2ings(self, ids):
+    return self._2(ids, self._id2ing)
   
-  def ids2unit(self):
-    pass
+  def ids2units(self, ids):
+    return self._2(ids, self._id2unit)
 
-  def ids2words(self):
-    pass
+  def ids2words(self, ids):
+    return self._2(ids, self._id2word)
+
+  def _2(self, names, d):
+    if type(names) in [list, range, np.ndarray]:
+      return [d.get(n, 0) for n in names]
+    else:
+      return d.get(names, 0)
 
   def get_ingredient_batch(self, batchsize=25, max_ingredients=15):
     count = 0
-    batch = np.zeros((batchsize, max_ingredients), dtype=np.int16)
-    for doc in self.coll.find({},
+    batch = np.zeros((batchsize, max_ingredients), dtype=np.float32)
+    for doc in self.coll.sample({},
                               {"ingredients.foodId":1, "_id":0},
                               no_cursor_timeout=True
                               ):
@@ -58,4 +80,4 @@ class Recipes:
       if count == batchsize:
           yield batch
           count = 0
-          batch = np.zeros((batchsize, max_ingredients), dtype=np.int16)
+          batch = np.zeros((batchsize, max_ingredients), dtype=np.float32)
